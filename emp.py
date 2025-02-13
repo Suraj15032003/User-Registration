2f86635de8163a3e4dcd0bf3882c0738ebfea6fc
Suraj15032003 <suraj1412.be21@chitkara.edu.in>
2025-02-13 11:15:11 +0530

    [Suraj] solving conflict

--- emp.py
+++ emp.py
@@ -1,19 +1,14 @@
 import re
-<<<<<<< HEAD
 
-def lastname(Last_name):
-    name_len=len(last_name)
-=======
 # use case 1
 def firstname(first_name):
     name_len=len(first_name)
->>>>>>> user_case_valid_firstname
     if name_len<3:
         print("Invalid name")
-    elif re.match(r'^[A-Z][a-zA-Z]*$', last_name):
-        print("Valid Lastname")
+    elif re.match(r'^[A-Z][a-zA-Z]*$', first_name):
+        print("Valid firsttname")
     else:
         print("Invalid Lastname")
 
-last_name = str(input("Enter your Lastname : "))
-lastname(last_name)
+first_name = str(input("Enter your firsttname : "))
+firstname(first_name)
