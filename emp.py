2e9f1e4b978877245309a556c672bc5e356b6fd5
Suraj15032003 <suraj1412.be21@chitkara.edu.in>
2025-02-13 11:34:10 +0530

    [Suraj] adding valid lasttname file

--- emp.py
+++ emp.py
@@ -11,3 +11,20 @@ def firstname(first_name):
 
 first_name = str(input("Enter your First name "))
 firstname(first_name)
+
+
+
+
+def lastname(last_name):
+    name_len=len(last_name)
+    if name_len<3:
+        print("Invalid name")
+    elif re.match(r'^[A-Z][a-zA-Z]*$', last_name):
+        print("valid name")
+    else:
+        print("Invalid name")
+
+last_name = str(input("Enter your last name: "))
+lastname(last_name)
+
+
