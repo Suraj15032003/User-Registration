9afa1ec9701e33cd4c30e978187973318f5d64d8
Suraj15032003 <suraj1412.be21@chitkara.edu.in>
2025-02-13 10:43:54 +0530

    [Suraj] adding valid first_name code emp.py

---
+++ emp.py
@@ -0,0 +1,13 @@
+import re
+
+def firstname(first_name):
+    name_len=len(first_name)
+    if name_len<3:
+        print("Invalid name")
+    elif re.match(r'^[A-Z][a-zA-Z]*$', first_name):
+        print("valid Name")
+    else:
+        print("Invalid")
+
+first_name = str(input("Enter your First name "))
+firstname(first_name)
